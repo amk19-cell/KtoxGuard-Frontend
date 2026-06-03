7d4286eb5284a5f057b48be6caccbd7ab33e478e
amk19-cell <doamk59@gmail.com>
2026-06-03 01:14:33 +0000

    Create i18n.js

---
+++ src/i18n.js
@@ -0,0 +1,28 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import fr from './locales/fr.json';
+import en from './locales/en.json';
+import ko from './locales/ko.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      ko: { translation: ko },
+      fr: { translation: fr },
+    },
+    fallbackLng: 'en',
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
