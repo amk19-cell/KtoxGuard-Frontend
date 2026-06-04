3312ea4ca9165fb8264a4312a8eb637dfc9eb5e8
amk19-cell <doamk59@gmail.com>
2026-06-04 06:48:41 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Layout from './components/Layout'
-import AdminDashboard from './dashboards/agency/AdminDashboard'
-// Les autres dashboards seront ajoutés plus tard
-
-// Simulation d'authentification (à remplacer par votre vraie logique)
-const mockAuth = {
-  role: 'agency',
-  sub_role: 'admin', // admin, manager, artist
-}
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout';
+import AdminDashboard from './dashboards/agency/AdminDashboard';
+import ArtistDetailView from './dashboards/agency/ArtistDetailView';
 
 function App() {
-  // Redirection selon le rôle
-  const getDashboard = () => {
-    if (mockAuth.role !== 'agency') return <Navigate to="/login" />
-    switch (mockAuth.sub_role) {
-      case 'admin': return <AdminDashboard />
-      case 'manager': return <div>ManagerDashboard (à venir)</div>
-      case 'artist': return <div>ArtistDashboard (à venir)</div>
-      default: return <Navigate to="/login" />
-    }
-  }
-
+  const [artists, setArtists] = useState([]);
+  useEffect(() => {
+    fetch('https://ktoxguard-api-production.up.railway.app/artists')
+      .then(res => res.json())
+      .then(setArtists)
+      .catch(err => console.error(err));
+  }, []);
   return (
-    <Layout userRole={mockAuth.sub_role}>
+    <Layout artists={artists}>
       <Routes>
-        <Route path="/" element={getDashboard()} />
-        <Route path="/dashboard/agency" element={getDashboard()} />
-        <Route path="/dashboard/agency/artist/:id" element={<div>Détail artiste (à venir)</div>} />
+        <Route path="/" element={<AdminDashboard />} />
+        <Route path="/artist/:id" element={<ArtistDetailView />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
