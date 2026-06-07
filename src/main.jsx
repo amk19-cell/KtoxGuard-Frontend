02f7baa07263c372e6e4039c9e3dbe3b7cc6c5b2
amk19-cell <doamk59@gmail.com>
2026-06-07 15:06:50 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './i18n';
 import App from './App';
-import ErrorBoundary from './components/ErrorBoundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <App />
   </React.StrictMode>
 );
